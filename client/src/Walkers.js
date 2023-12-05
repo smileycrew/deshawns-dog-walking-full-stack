738d3aa1bbea6a97337899f4c19c5932266f455f
Edwin Moz <edwin.moz@icloud.com>
2023-12-05 09:47:41 -0600

    initial push

---
+++ client/src/Walkers.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react"
+
+export const Walkers = () => {
+    const [walkers, setWalkers] = useState([])
+    useEffect(() => {
+        ''
+    }, [])
+    return (
+        <>hi</>
+    )
+}
