b23fa490b442266cc23462418df1e620445f0bb6
Edwin Moz <edwin.moz@icloud.com>
2023-12-07 15:30:32 -0600

    added the endpoint to get the walker and his cities ran into an issue which is all http requests require a status

--- Models/Walker.cs
+++ Models/Walker.cs
@@ -4,4 +4,5 @@ public class Walker
     public int Id { get; set; }
     public string Name { get; set; }
     public string ImageURL { get; set; }
+    public List<WalkerCity> WalkerCities { get; set; }
 }
